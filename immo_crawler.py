6d7c6b89b3656361e5d38d6b6535fa37f4d32b14
cichons <cichons@cichon.server>
2019-09-15 20:25:36 +0200

    typos

--- immo_crawler.py
+++ immo_crawler.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 from bs4 import BeautifulSoup
-import time
 import requests
 from fake_useragent import UserAgent
 import random
 import pandas as pd
 import re
-from tqdm import tqdm
 import sqlite3
 import datetime
-import numpy as np
 import multiprocessing
-
+from pebble import ProcessPool
+from concurrent.futures import TimeoutError
 
 
 
@@ -24,16 +22,17 @@ import multiprocessing
 # get already known Expose IDs from DB
 #######################################
 
-conn = sqlite3.connect("sqlite/realty.db")
+conn = sqlite3.connect("/home/cichons/immo_scraper/sqlite/realty.db")
 c = conn.cursor()
 try:
-    c.execute("SELECT distinct expose_id FROM realty_meta;")
+    c.execute("SELECT distinct id FROM realties;")
     x = c.fetchall()
     known_expose_ids = [i[0] for i in x]
 except:
     known_expose_ids = []
 conn.close()
 
+print('Currently there are ', len(known_expose_ids), 'realties stored in DB.')
 
 
 
@@ -63,6 +62,9 @@ def get_proxies():
                 proxies_https.append(https)
 
     return proxies_http, proxies_https
+
+
+
 
 
 
@@ -108,7 +110,7 @@ def parallelize_function(url_list, func):
     start_ts = datetime.datetime.now()
   
     num_cores = multiprocessing.cpu_count()-1  #leave one free to not freeze machine
-    print('processinc function on', num_cores, 'cores...')
+    print('processing function on', num_cores, 'cores...')
     pool = multiprocessing.Pool(num_cores)
     results = pool.map(func, url_list)
     pool.close()
@@ -119,14 +121,21 @@ def parallelize_function(url_list, func):
     return results
 
 
-"""
-    expose_list = list(set([link.get('href').replace('/expose/', '') for link in soup.find_all('a') if "/expose/" in link.get('href')]))
-    adresses = [location.getText() for location in soup.findAll("button", {"class": "button-link link-internal result-list-entry__map-link", "title": "Auf der Karte anzeigen"})]
+def parallelize_function_timeout(urls, func):
+    with ProcessPool() as pool:
+        future = pool.map(func, urls, timeout=90)
 
-    city_county = [add.split(', ')[-1] for add in adresses]
-    city_quarter = [add.split(', ')[-2] for add in adresses]
-    street = [add.split(', ')[-3] if len(add.split(', ')) == 3 else ''  for add in adresses]
-"""
+        iterator = future.result()
+        result = pd.DataFrame()
+        while True:
+            try:
+                result = result.append(next(iterator))
+            except StopIteration:
+                break
+            except TimeoutError as error:
+                print("scraping took longer than %d seconds" % error.args[1])
+        return result
+
 
 def scrape_meta_chunk(url, return_soup=False):
     print(url)
@@ -143,7 +152,16 @@ def scrape_meta_chunk(url, return_soup=False):
     
     try:
         
-        data = pd.DataFrame([[location['data-result-id'], location.getText().split(', ')[-1], location.getText().split(', ')[-2], location.getText().split(', ')[-3] if len(location.getText().split(', ')) == 3 else ''] for location in soup.findAll("button", {"class": "button-link link-internal result-list-entry__map-link", "title": "Auf der Karte anzeigen"})], columns=['id','city_county', 'city_quarter', 'street' ])
+        data = pd.DataFrame(
+            [
+            [location['data-result-id'], 
+             location.getText().split(', ')[-1], 
+             location.getText().split(', ')[-2], 
+             location.getText().split(', ')[-3] if len(location.getText().split(', ')) == 3 else '', 
+             str(datetime.datetime.now())] 
+            for location in 
+            soup.findAll("button", {"class": "button-link link-internal result-list-entry__map-link", "title": "Auf der Karte anzeigen"})], columns=['id','city_county', 'city_quarter', 'street', 'scraped_ts']
+        )
         #data = pd.DataFrame({'id':expose_list,'city_county':city_county, 'city_quarter': city_quarter, 'street':street, 'scraped_ts': str(datetime.datetime.now())})
         
     except Exception as err:
@@ -158,8 +176,6 @@ def scrape_meta_chunk(url, return_soup=False):
 
 def scrape_details_chunk(realty):
         
-    
-
     expose_id = []
     title = []
     realty_type = []
@@ -282,7 +298,7 @@ def scrape_details_chunk(realty):
             pass
         
     results = pd.DataFrame({
-    'expose_id': expose_id,
+    'id': expose_id,
     'title': title, 
     'realty_type': realty_type, 
     'floor': floor,
@@ -337,9 +353,9 @@ ua = UserAgent(fallback='Mozilla/5.0 (compatible; Googlebot/2.1; ')
 url = 'https://www.immobilienscout24.de/Suche/S-T/P-1/Wohnung-Miete/Umkreissuche/Hamburg/-/1840/2621814/-/-/30'
 
 
-realty_meta, soup = scrape_meta_chunk(url,return_soup=True)
+realty_meta_df, soup = scrape_meta_chunk(url,return_soup=True)
 num_pages = len(soup.find_all('option'))
-print('scraped', len(realty_meta), 'realties on first page')
+print('scraped', len(realty_meta_df), 'realties on first page')
 print('found', num_pages, 'pages to scrape')
 
 
@@ -352,29 +368,34 @@ print('found', num_pages, 'pages to scrape')
 
 
 realty_meta_urls = ['https://www.immobilienscout24.de/Suche/S-T/P-' + str(page) + '/Wohnung-Miete/Umkreissuche/Hamburg/-/1840/2621814/-/-/30' for page in range(2, num_pages + 1)]
-realty_meta = parallelize_function(realty_meta_urls, scrape_meta_chunk)
-realty_meta_df = pd.concat(realty_meta, ignore_index=True)
+realty_meta_df = realty_meta_df.append(parallelize_function_timeout(realty_meta_urls, scrape_meta_chunk))
+#realty_meta_df = pd.concat(realty_meta, ignore_index=True)
 print('Done')
  
     
     
 
     
-    
+
+
+# In[4]:
+
+
 # drop known exposes:
 #######################################
 
 
-print('removing', len(realty_meta_df) - len(realty_meta_df.loc[~realty_meta_df.id.isin(known_expose_ids)]), 'already scraped Exposes from list.')
-realty_meta_df = realty_meta_df.loc[~realty_meta_df.id.isin(known_expose_ids)]
+
+print('removing', len(realty_meta_df.loc[pd.to_numeric(realty_meta_df.id).isin(known_expose_ids)]), 'already scraped Exposes from list.')
+realty_meta_df = realty_meta_df.loc[~pd.to_numeric(realty_meta_df.id).isin(known_expose_ids)] #realty_meta_df.loc[~realty_meta_df.id.isin(known_expose_ids)]
 realty_meta_df = realty_meta_df.drop_duplicates()
 realty_meta_df = realty_meta_df.reset_index(drop=True)
+print(len(realty_meta_df), 'realties left.')
 
 
 
 
-
-# In[4]:
+# In[5]:
 
 
 #######################################
@@ -435,9 +456,10 @@ realty_details = pd.DataFrame(columns=[
 
 realties = list(set(list(realty_meta_df.id)))
 print('scraping', len(realties), 'realties...')
-realty_details = parallelize_function(realties, scrape_details_chunk)
-realty_details_df = pd.concat(realty_details, ignore_index=True)
-
+realty_details_df = parallelize_function_timeout(realties, scrape_details_chunk)
+#realty_details_df = pd.concat(realty_details, ignore_index=True)
+realty_details_df = realty_details_df.drop_duplicates()
+realty_details_df = realty_details_df.reset_index(drop=True)
 print('scraped', len(realty_details_df), 'realties.')
 
 
@@ -448,54 +470,28 @@ print('scraped', len(realty_details_df), 'realties.')
 
 
 
-# In[ ]:
+# In[6]:
 
 
-realty_details_df_backup = realty_details_df
+realty_meta_df['id'] = realty_meta_df['id'].astype(int)
+realty_details_df['id'] = realty_details_df['id'].astype(int)
+
+final_data = realty_meta_df.drop('scraped_ts',axis = 1).merge(realty_details_df, on='id', suffixes = ['_meta', '_details'])
 
 
-# In[ ]:
+# In[7]:
 
 
-# convert data types
-#######################################
-realty_details_df.square_m = realty_details_df.square_m.fillna('999').apply(lambda x: re.sub('[^0-9]','', str(x).replace(',','.')))
-realty_details_df.num_rooms = realty_details_df.num_rooms.fillna('999').apply(lambda x: x.replace(',','.'))
-realty_details_df.year_built = realty_details_df.year_built.fillna('999').apply(lambda x: re.sub('[^0-9]','',str(x)))
-realty_details_df.last_refurb = realty_details_df.last_refurb.fillna('999').apply(lambda x: re.sub('[^0-9]','',str(x)))
-realty_details_df.energy_consumption = realty_details_df.energy_consumption.fillna('999').apply(lambda x: re.sub('[^0-9]','',x.replace(',','.')))
-realty_details_df.net_rent = realty_details_df.net_rent.fillna('999').apply(lambda x: re.sub('[^0-9]','',x.replace(',','.')))
-realty_details_df.gross_rent = realty_details_df.gross_rent.fillna('999').apply(lambda x: re.sub('[^0-9]','',x.replace(',','.')))
-realty_details_df.scraped_ts = realty_details_df.fillna('999').scraped_ts
-
-realty_meta_df.scraped_ts = pd.to_datetime(realty_meta_df.scraped_ts)
+final_data.head()
 
 
-#cols = realty_details_df.columns.drop('expose_id')
-#realty_details_df[cols] = realty_details_df[cols].apply(pd.to_numeric, errors='ignore')
+# In[8]:
 
 
-# In[ ]:
+print(len(final_data))
 
 
-realty_details_df.columns.drop('expose_id')
-
-
-# In[ ]:
-
-
-cols = ['expose_id', 'square_m', 'storage_m',       'num_rooms', 'num_bedrooms', 'num_baths', 'num_carparks', 'year_built', 'energy_consumption','net_rent', 'gross_rent']
-realty_details_df[cols] = realty_details_df[cols].apply(pd.to_numeric, errors='ignore')
-realty_details_df.scraped_ts = pd.to_datetime(realty_details_df.scraped_ts)
-
-
-# In[ ]:
-
-
-realty_details_df.dtypes
-
-
-# In[ ]:
+# In[9]:
 
 
 #######################################
@@ -504,11 +500,10 @@ realty_details_df.dtypes
 
 
 print('Writing results to DB.')
-conn = sqlite3.connect("sqlite/realty.db")
+conn = sqlite3.connect("/home/cichons/immo_scraper/sqlite/realty.db")
 
-
-realty_meta_df.to_sql('realty_meta', conn, if_exists ='append')
-realty_details_df.to_sql('realty_details', conn, if_exists ='append')
+final_data.to_sql('realties', conn, if_exists ='append')
 
 conn.close()
 print('done.')
+
